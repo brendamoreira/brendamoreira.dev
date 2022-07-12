adb5eb33e52c08bebe95aba968aea62fb2c7405d
Brenda Moreira <brendanatalia94@hotmail.com>
2022-07-12 14:10:11 +0200

    import font family

--- src/App.vue
+++ src/App.vue
@@ -17,14 +17,14 @@ import Logo from "./components/Logo.vue";
 
 <style>
 @import "./assets/base.css";
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
 
 #app {
   width: 100vw;
   height: 100vh;
   margin: 0;
   padding: 2rem;
-
-  font-weight: normal;
+  font-family: 'Open Sans', sans-serif;
 }
 .main {
   margin: 0 auto;
