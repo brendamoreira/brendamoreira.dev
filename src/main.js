5872008b9220bb2ed8a8a732257617436ed259ff
Brenda Moreira <brendanatalia94@hotmail.com>
2022-07-12 13:20:20 +0200

    add font for icons

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faGithub, faLinkedin, faGitlab } from '@fortawesome/free-brands-svg-icons'
+
 import router from "./router"
+
+library.add(faGithub, faLinkedin, faGitlab)
 
 const app = createApp(App);
 app.use(router);
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
