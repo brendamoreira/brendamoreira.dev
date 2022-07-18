ca1c6a1e419f2c2399ec0fe6d149fe6252303668
Brenda Moreira <brendanatalia94@hotmail.com>
2022-07-18 13:38:15 +0200

    add mock json that dynamically displays

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,31 +1,41 @@
 <template>
-  <div class="card">
-    <div class="title">Test</div>
-    <div class="description">
-      about the project Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-      ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-      ex ea commodo consequat."
+  <div class="cards-holder">
+    <div class="card" v-for="project in projects" :key="projects.name">
+      <div class="title">{{project.name}}</div>
+      <div class="description">
+        {{project.description}}
+      </div>
     </div>
   </div>
 </template>
 <script>
-export default{
-    name: "Cards"
-}</script>
+import data from "../data.json";
+export default {
+  name: "Cards",
+  data() {
+    return {
+      projects: data,
+    };
+  },
+};
+</script>
 <style>
-.card{
-    background-color: #f0e7fb;
-    width: 400px;
-    height: 200px;
-    border-radius: 5px;
-    padding: 5px;
-    box-shadow: 0px 4px 4px rgba(51, 51, 51, 0.25);
-
+.cards-holder{
+  display: flex;
+  gap: 20px;
+  margin: auto;
 }
-.title{
-    font-size: 20px;
-    font-weight: bold;
-    margin: 5px;
+.card {
+  background-color: #f0e7fb;
+  width: 400px;
+  height: 200px;
+  border-radius: 5px;
+  padding: 5px;
+  box-shadow: 0px 4px 4px rgba(51, 51, 51, 0.25);
+}
+.title {
+  font-size: 20px;
+  font-weight: bold;
+  margin: 5px;
 }
 </style>
