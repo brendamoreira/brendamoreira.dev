9af431eeec012fc038793b01fa68fe03cae79055
Brenda Moreira <brendanatalia94@hotmail.com>
2022-07-19 14:50:12 +0200

    update project card style

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -22,6 +22,7 @@ export default {
 <style>
 .cards-holder{
   display: flex;
+  flex-wrap: wrap;
   gap: 20px;
   margin: auto;
 }
@@ -37,5 +38,8 @@ export default {
   font-size: 20px;
   font-weight: bold;
   margin: 5px;
+}
+.description{
+ padding: 5px;
 }
 </style>
