4e58a6f081c75d499fb2c92c6da63b19bf12ef71
Brenda Moreira <brendanatalia94@hotmail.com>
2022-07-20 15:09:41 +0200

    update opensource page

--- src/pages/OpenSource.vue
+++ src/pages/OpenSource.vue
@@ -1,1 +1,29 @@
-<template>Open Source</template>
+<template>
+  <div class="opensource">
+    <h2 class="os-title highlight-text">AcompanhaLegis</h2>
+    <p class="os-description">
+      AcompanhaLegis is an open source project I’ve been working on. It’s a
+      brazilian politician tracker, it’s made with VueJS and NuxtJS. It’s a very
+      nice project which I could enroll, the production version is currently
+      hosted at AWS and can be viewed here:
+      <a class="link" href="https://acompanhalegis.info"
+        >https://acompanhalegis.info</a
+      >
+    </p>
+    <h2 class="os-title highlight-text">BOSS (Big Open Source Sibling)</h2>
+    <p class="os-description"></p>
+  </div>
+</template>
+<style scoped>
+.os-title {
+  margin: 10px;
+}
+.os-description {
+  margin: 10px;
+  margin-bottom: 20px;
+}
+.link,
+link:visited {
+  color: var(--primary-color);
+}
+</style>
